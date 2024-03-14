8b783c0c363e52fff0692304fde2b4eb99bc0f6c
AcekBecek <acekmegamen16@gmail.com>
2024-03-14 16:40:57 +0700

    Active Timesheet Approval
    List of Timesheet Need to Approve

--- force-app/main/default/lwc/viewActiveTimesheetApproval/viewActiveTimesheetApproval.js
+++ force-app/main/default/lwc/viewActiveTimesheetApproval/viewActiveTimesheetApproval.js
@@ -1,3 +1,91 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import { gql, graphql } from "lightning/uiGraphQLApi";
+import ConvertApproverName from "@salesforce/apex/lwc_ApprovalTimesheetController.ApproverName"
 
-export default class ViewActiveTimesheetApproval extends LightningElement {}
+export default class ViewActiveTimesheetApproval extends LightningElement {
+
+    results
+    errors
+    @api recordId
+    ApproverName
+
+    connectedCallback(){
+        ConvertApproverName({ recordPageId : this.recordId})
+        .then((res)=>{
+            this.ApproverName = res;
+        })
+    }
+
+    @wire(graphql, {
+        query: gql`
+            query timesheets($ApproverName : String){
+                uiapi{
+                    query{
+                        Timesheet__c(where: {Timesheet_Approver__r :{Name : { eq: $ApproverName}}})
+                        {
+                            edges{
+                                node{
+                                    Id
+                                    Name{
+                                        value
+                                    }
+                                    Employee__r{
+                                        Name{
+                                            value
+                                        }
+                                        Employee_ID__c{
+                                            value
+                                        }
+                                    }
+                                    Project__r{
+                                        Name{
+                                            value
+                                        }
+                                        SPK__c{
+                                            value
+                                        }
+                                    }
+                                    Start_Date__c{
+                                        value
+                                    }
+                                    End_Date__c{
+                                        value
+                                    }
+                                    Time__c{
+                                        value
+                                    }
+                                    Remarks__c{
+                                        value
+                                    }
+                                    Approval_Status__c{
+                                        value
+                                    }
+                                    Timesheet_Approver__r{
+                                        Name{
+                                            value
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        `,
+        variables: "$variables",
+    })
+    graphqlQueryResult({ data, errors }) {
+        if (data) {
+          this.results = data.uiapi.query.Timesheet__c.edges.map((edge) => edge.node);
+        }
+        this.errors = errors;
+    }
+
+    get variables() {
+        return {
+          ApproverName: this.ApproverName,
+        };
+      }
+    
+
+}
