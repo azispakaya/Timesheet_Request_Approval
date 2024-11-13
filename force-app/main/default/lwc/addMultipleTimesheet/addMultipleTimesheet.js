4c1187979d917b9d6cccdaf024ff991814ff9fa0
AcekBecek <nurazispakaya16@gmail.com>
2024-11-13 15:08:06 +0700

    add function refreshApex

--- force-app/main/default/lwc/addMultipleTimesheet/addMultipleTimesheet.js
+++ force-app/main/default/lwc/addMultipleTimesheet/addMultipleTimesheet.js
@@ -6,7 +6,7 @@
  * @author [AcekBecek]
  * @email [nurazispakaya16@mail.com]
  * @create date 2024-03-24 15:40:38
- * @modify date 2024-11-13 15:02:11
+ * @modify date 2024-11-13 15:06:40
  * @desc [Controller for Add multiple Timehseet]
  */
 import { LightningElement, api, track, wire } from "lwc";
@@ -22,6 +22,7 @@ import createMultiTimesheet from "@salesforce/apex/lwc_ApprovalTimesheetControll
 import FORM_FACTOR from "@salesforce/client/formFactor";
 import convertCampaign from "@salesforce/apex/lwc_RequestTimesheetController.convertCampaign";
 import { getRecord } from "lightning/uiRecordApi";
+import { refreshApex } from "@salesforce/apex";
 
 export default class AddMultipleTimesheet extends LightningElement {
   @api recordId;
@@ -653,7 +654,7 @@ export default class AddMultipleTimesheet extends LightningElement {
         this.toast("Successfully Request Timesheet", "success", "Success");
         this.dispatchEvent(new CloseActionScreenEvent());
         this.isLoading = false;
-        await this.wiredEmployeeResult();
+        await refreshApex(this.wiredEmployeeResult);
       } else {
         this.toast(
           `Failed to request timesheet with error: ${resMSG.split(":")[1]}`,
